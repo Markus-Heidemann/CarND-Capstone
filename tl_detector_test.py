583a093e853dda01c153c1b89222124618f060d1
Markus Heidemann <marheidemann@googlemail.com>
2019-04-19 00:02:23 +0200

    added image_exporter.launch added Hough transform to tl_detector_test

--- tl_detector_test.py
+++ tl_detector_test.py
@@ -6,11 +6,12 @@ from PIL import ImageDraw
 from PIL import ImageColor
 import time
 from scipy.stats import norm
+import cv2 as cv
 
 #%matplotlib inline
 plt.style.use('ggplot')
 
-SSD_GRAPH_FILE_V2 = '/home/markus/Udacity/models/ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb'
+SSD_GRAPH_FILE_V1 = '/home/markus/Udacity/models/ssd_mobilenet_v1_coco_2018_01_28/frozen_inference_graph.pb'
 SSD_GRAPH_FILE = '/home/markus/Udacity/models/ssd_mobilenet_v1_coco_11_06_2017/frozen_inference_graph.pb'
 RFCN_GRAPH_FILE = '/home/markus/Udacity/models/rfcn_resnet101_coco_11_06_2017/frozen_inference_graph.pb'
 FASTER_RCNN_GRAPH_FILE = '/home/markus/Udacity/models/faster_rcnn_inception_resnet_v2_atrous_coco_11_06_2017/frozen_inference_graph.pb'
@@ -29,7 +30,7 @@ def filter_boxes(min_score, boxes, scores, classes):
     n = len(classes)
     idxs = []
     for i in range(n):
-        if scores[i] >= min_score:
+        if scores[i] >= min_score and classes[i] == 10:
             idxs.append(i)
 
     filtered_boxes = boxes[idxs, ...]
@@ -76,6 +77,58 @@ def draw_boxes_np(image, boxes, classes, thickness=4):
     # image = np.expand_dims(image, 0)
     return image
 
+def crop_box(image, box):
+    bottom, left, top, right = box[...]
+    return image.crop((int(left), int(bottom), int(right), int(top)))
+
+def draw_circles(img):
+    img = np.asarray(img)
+    height = img.shape[0]
+    img = img[:, :, ::-1].copy() # convert to BGR
+    gimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    gimg = cv.medianBlur(gimg, 5)
+    circles = cv.HoughCircles(gimg, cv.HOUGH_GRADIENT, 1, int(height * 0.25),
+                            param1=50, param2=30, minRadius=0, maxRadius=0)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))[0]
+        assert(len(circles) == 3)
+        print(circles)
+
+        top_idx = np.argmin(circles[:,1])
+        top_circ = circles[top_idx]
+        circles = np.delete(circles, top_idx, axis=0)
+
+        mid_idx = np.argmin(circles[:,1])
+        mid_circ = circles[mid_idx]
+        circles = np.delete(circles, mid_idx, axis=0)
+
+        bot_circ = circles[0]
+
+        # for i in circles[0,:]:
+        #     # draw the outer circle
+        #     cv.circle(img, (i[0], i[1]), i[2], (0,255,0), 2)
+        #     # draw the center of the circle
+        #     # cv.circle(img,(i[0],i[1]),2,(0,0,255),3)
+
+        cv.circle(img, (top_circ[0], top_circ[1]), top_circ[2], (0,0,255), 2)
+        cv.circle(img, (mid_circ[0], mid_circ[1]), mid_circ[2], (255,255,0), 2)
+        cv.circle(img, (bot_circ[0], bot_circ[1]), bot_circ[2], (0,255,0), 2)
+
+        print(top_circ)
+        print(mid_circ)
+        print(bot_circ)
+
+        print()
+
+        print(img[top_circ[1], top_circ[0], :])
+        print(img[mid_circ[1], mid_circ[0], :])
+        print(img[bot_circ[1], bot_circ[0], :])
+
+    img = img[:, :, ::-1].copy() # convert back to RGB
+    img = Image.fromarray(img)
+
+    return img
+
 def load_graph(graph_file):
     """Loads a frozen inference graph"""
     graph = tf.Graph()
@@ -107,7 +160,9 @@ def main():
     detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 
     # Load a sample image.
-    image = Image.open('/home/markus/workspace/CarND-Object-Detection-Lab/assets/sample1.jpg')
+    # img_path = '/home/markus/workspace/CarND-Object-Detection-Lab/assets/Traffic-Signals.jpg'
+    img_path = '/home/markus/.ros/frame0000.jpg'
+    image = Image.open(img_path)
     image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
 
     with tf.Session(graph=detection_graph) as sess:
@@ -120,7 +175,7 @@ def main():
         scores = np.squeeze(scores)
         classes = np.squeeze(classes)
 
-        confidence_cutoff = 0.8
+        confidence_cutoff = 0.5
         # Filter boxes with a confidence score less than `confidence_cutoff`
         boxes, scores, classes = filter_boxes(confidence_cutoff, boxes, scores, classes)
 
@@ -130,10 +185,14 @@ def main():
         box_coords = to_image_coords(boxes, height, width)
 
         # Each class with be represented by a differently colored box
-        draw_boxes(image, box_coords, classes)
+        # draw_boxes(image, box_coords, classes)
 
-        plt.figure(figsize=(12, 8))
-        plt.imshow(image)
+        # plt.figure(figsize=(12, 8))
+        # plt.imshow(image)
+
+        max_score_idx = np.argmax(scores)
+        image = crop_box(image, box_coords[max_score_idx])
+        image = draw_circles(image)
         image.save('./result.png')
 
 if __name__ == '__main__':
